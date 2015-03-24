3d6b47bf9d8513f40472f5f6b3caefffb85355f0
Álex Hernández <alex.hdz94@gmail.com>
2015-03-24 13:22:30 +0100

    Mejorada la manera en la que se muestran las facturas (COLORES)

--- app/src/main/java/es/gk2/janhout/gk2_android/AdaptadorListaFacturas.java
+++ app/src/main/java/es/gk2/janhout/gk2_android/AdaptadorListaFacturas.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -33,21 +34,31 @@ public class AdaptadorListaFacturas extends ArrayAdapter<Factura> {
         if (convertView == null) {
             convertView = inflador.inflate(recurso, null);
             vh = new ViewHolder();
+            vh.filaFactura = (RelativeLayout) convertView.findViewById(R.id.filaFactura);
             vh.numeroFactura = (TextView) convertView.findViewById(R.id.numeroFactura);
             vh.fechaFactura = (TextView) convertView.findViewById(R.id.fechaFactura);
             vh.importeFactura = (TextView) convertView.findViewById(R.id.importeFactura);
             vh.importePagado = (TextView) convertView.findViewById(R.id.importePagado);
+
+            if(datos.get(position).getEstadoFactura() == 0)
+                vh.filaFactura.setBackgroundColor(contexto.getResources().getColor(R.color.rojo));
+            else if(datos.get(position).getEstadoFactura() == 1)
+                vh.filaFactura.setBackgroundColor(contexto.getResources().getColor(R.color.verde));
+            else if(datos.get(position).getEstadoFactura() == 2)
+                vh.filaFactura.setBackgroundColor(contexto.getResources().getColor(R.color.amarillo));
             vh.acciones = (Spinner) convertView.findViewById(R.id.sp_acciones_facturas);
+
+            convertView.setTag(vh);
         } else {
             vh = (ViewHolder) convertView.getTag();
         }
 
         vh.numeroFactura.setText(datos.get(position).getNumeroFactura());
         vh.fechaFactura.setText(datos.get(position).getFechaFactura().toString());
-        vh.importeFactura.setText(Float.toString(datos.get(position).getImporteFactura()));
-        vh.importePagado.setText(Float.toString(datos.get(position).getImportePagado()));
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(contexto, R.array.lista_acciones, android.R.layout.simple_spinner_item);
+        vh.importeFactura.setText("Importe: "+Float.toString(datos.get(position).getImporteFactura()));
+        if(datos.get(position).getEstadoFactura() == 0)
+            vh.importePagado.setText("Pagado: "+Float.toString(datos.get(position).getImportePagado()));
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(contexto, R.array.lista_acciones_facturas, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         vh.acciones.setAdapter(adapter);
         return convertView;
@@ -55,6 +66,7 @@ public class AdaptadorListaFacturas extends ArrayAdapter<Factura> {
 
     public static class ViewHolder {
         public Integer estadoFactura;
+        public RelativeLayout filaFactura;
         public TextView numeroFactura;
         public TextView fechaFactura;
         public TextView importeFactura;
--- app/src/main/java/es/gk2/janhout/gk2_android/Factura.java
+++ app/src/main/java/es/gk2/janhout/gk2_android/Factura.java
@@ -5,22 +5,18 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * Created by usuario on 23/03/2015.
  */
 public class Factura {
     private String numeroFactura;
-    private Date fechaFactura;
+    private String fechaFactura;
     // 0: impagada | 1: pagada | 2: borrador
     private Integer estadoFactura;
     private Float importeFactura;
     private Float importePagado;
 
-    public Factura(String numeroFactura, Date fechaFactura, Integer estadoFactura, Float importeFactura, Float importePagado) {
+    public Factura(String numeroFactura, String fechaFactura, Integer estadoFactura, Float importeFactura, Float importePagado) {
         this.numeroFactura = numeroFactura;
         this.fechaFactura = fechaFactura;
         this.estadoFactura = estadoFactura;
@@ -28,23 +24,22 @@ public class Factura {
         this.importePagado = importePagado;
     }
 
-    public Factura(JSONObject facturaJSON){
+    public Factura(JSONObject facturaJSON) {
         try {
             this.numeroFactura = facturaJSON.getString("NUMERO");
-            this.fechaFactura = new SimpleDateFormat("YYYY-NN-DD HH:MM:SS").parse(facturaJSON.getString("FECHA"));
+            String stringFecha = facturaJSON.getString("FECHA");
+            this.fechaFactura = stringFecha.substring(8, 10)+"/"+stringFecha.substring(5, 7)+"/"+stringFecha.substring(0, 4);
             String estadoFacturaTemp = facturaJSON.getString("ESTADO");
-            if(estadoFacturaTemp == "Impagada")
+            if (estadoFacturaTemp.equals("Impagada"))
                 this.estadoFactura = 0;
-            else if(estadoFacturaTemp == "Pagada")
+            else if (estadoFacturaTemp.equals("Pagada"))
                 this.estadoFactura = 1;
-            else if(estadoFacturaTemp == "Borrador")
+            else if (estadoFacturaTemp.equals("Borrador"))
                 this.estadoFactura = 2;
             this.importeFactura = Float.parseFloat(facturaJSON.getString("LIQUIDO"));
             this.importePagado = Float.parseFloat(facturaJSON.getString("PENDIENTE"));
         } catch (JSONException e) {
             Log.e("error mio", e.toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
     }
 
@@ -56,11 +51,11 @@ public class Factura {
         this.numeroFactura = numeroFactura;
     }
 
-    public Date getFechaFactura() {
+    public String getFechaFactura() {
         return fechaFactura;
     }
 
-    public void setFechaFactura(Date fechaFactura) {
+    public void setFechaFactura(String fechaFactura) {
         this.fechaFactura = fechaFactura;
     }
 
