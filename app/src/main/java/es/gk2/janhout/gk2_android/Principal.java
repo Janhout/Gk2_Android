497b36d475c4272be0c345377c9fd4e418e4f5b4
Álex Hernández <alex.hdz94@gmail.com>
2015-03-24 17:10:47 +0100

    Fixes

--- app/src/main/java/es/gk2/janhout/gk2_android/Principal.java
+++ app/src/main/java/es/gk2/janhout/gk2_android/Principal.java
@@ -1,10 +1,9 @@
 package es.gk2.janhout.gk2_android;
 
 import android.app.Fragment;
-import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -29,9 +28,6 @@ public class Principal extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
-        setSupportActionBar(toolbar);
-
         titulos = getResources().getStringArray(R.array.lista_navigation_drawer);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -44,35 +40,6 @@ public class Principal extends ActionBarActivity {
         drawerList.setAdapter(new AdaptadorListaNavigationDrawer(this, R.layout.detelle_elemento_drawer, items));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-
-        drawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.bienvenido,
-                R.string.usuario
-        ) {
-            public void onDrawerClosed(View view) {
-                toolbar.setTitle("item");
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                toolbar.setTitle("hola");
-            }
-        };
-        drawerLayout.setDrawerListener(drawerToggle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -88,9 +55,6 @@ public class Principal extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
